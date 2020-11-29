3655bb364422b4f56f5927a8ecb342fed5d397ba
Kristen Steele <ksteelephoto@gmail.com>
2020-11-28 19:59:07 -0600

    first commit

---
+++ src/components/KandyKorner.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+export const KandyKorner = () => (
+    <>
+        <h2>Kandy Korner</h2>
+        <small>Kandy when you need a treat</small>
+        <address>
+            <div>Visit Us at the Nashville North Location</div>
+            <div>200 Kandykane Ln</div>
+        </address>
+    </>
+)
