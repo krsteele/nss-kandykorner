06a6c5dc4410a92cf38c71061d2dcdb1a32422bf
Kristen Steele <ksteelephoto@gmail.com>
2020-12-01 13:44:35 -0600

    call ProductList function and wrap it in ProductProvider

--- src/components/KandyKorner.js
+++ src/components/KandyKorner.js
@@ -2,15 +2,21 @@ import React from "react"
 import { LocationList } from "./locations/LocationList"
 import { LocationProvider } from "./locations/LocationProvider"
 import "./KandyKorner.css"
+import { ProductList } from "./products/ProductList"
+import { ProductProvider } from "./products/ProductProvider"
 
 export const KandyKorner = () => (
     <>
         <h2>Kandy Korner</h2>
         <small>Kandy when you need a treat</small>
 
-        <h2>Locations</h2>
+        
         <LocationProvider>
             <LocationList />
         </LocationProvider>
+        
+        <ProductProvider>
+            <ProductList />
+        </ProductProvider>
     </>
 )
