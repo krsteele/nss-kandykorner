71c4f4181d6b4b26a73c980c42ac3d64b42a0318
Kristen Steele <ksteelephoto@gmail.com>
2020-12-01 15:30:31 -0600

    add and refactor components needed to show a nav bar on initial render and route to locations or products depending on chosen link

--- src/components/KandyKorner.js
+++ src/components/KandyKorner.js
@@ -1,22 +1,12 @@
 import React from "react"
-import { LocationList } from "./locations/LocationList"
-import { LocationProvider } from "./locations/LocationProvider"
+import { Route } from "react-router-dom"
+import { ApplicationViews } from "./ApplicationViews"
+import { NavBar } from "./nav/NavBar"
 import "./KandyKorner.css"
-import { ProductList } from "./products/ProductList"
-import { ProductProvider } from "./products/ProductProvider"
 
-export const KandyKorner = () => (
-    <>
-        <h2>Kandy Korner</h2>
-        <small>Kandy when you need a treat</small>
-
-        
-        <LocationProvider>
-            <LocationList />
-        </LocationProvider>
-        
-        <ProductProvider>
-            <ProductList />
-        </ProductProvider>
+export const KandyKorner = () => {
+   return <>
+        <NavBar />
+        <ApplicationViews />
     </>
-)
+}
